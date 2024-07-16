face9dd88eef77118782cce1ca03ac6dfcabb27d
Robbie Diaz <metaldrummer610@gmail.com>
2024-07-16 16:12:44 -0400

    Modifying linux setup script

--- setup.sh
+++ setup.sh
@@ -4,7 +4,13 @@
 if [[ -f "/opt/homebrew/bin/brew" ]]; then
 	brew install zoxide fzf fd bat git-delta eza tldr nvim tmux font-jetbrains-mono-nerd-font ripgrep
 else # On linux
-	apt install zoxide fzf fd bat delta eza tldr nvim tmux
+	sudo apt install zoxide fzf fd-find bat delta tldr neovim tmux
+
+  wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+  echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+  sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+  sudo apt update
+  sudo apt install -y eza
 fi
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
